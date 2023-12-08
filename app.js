1d22f7bc0674f2d5e474702d563ad15f5ceffcc1
YuriyOhii <uaogiy@gmail.com>
2023-12-08 12:51:30 +0200

    adds addMethod and getMovieById

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const invokeAction = async ({ action, id, ...data }) => {
     case "list":
       const result = await moviesServices.getAllMovies();
       return console.log(result);
+    case "getMovie":
+      const movie = await moviesServices.getMovieById(id);
+      return console.log(movie);
+      case "add":
+      const newMovie = await moviesServices.addMovie(data);
+      return console.log(newMovie);
   }
 };
 
-invokeAction({action: 'list'});
+invokeAction({ action: "list", director: 'yura', title: 'my title' });
--- movies/index.js
+++ movies/index.js
@@ -5,7 +5,25 @@ import { nanoid } from "nanoid";
 
 const moviesPath = path.join("movies", "movies.json");
 
+const reWriteMovies = async (data) => {
+  await fs.writeFile(moviesPath, data);
+};
+
 export const getAllMovies = async () => {
   const data = await fs.readFile(moviesPath);
   return JSON.parse(data);
+};
+
+export const getMovieById = async (id) => {
+  const movies = await getAllMovies();
+  const movie = movies.find((el) => el.id === id);
+  return movie || null;
+};
+
+export const addMovie = async (data) => {
+  const movies = await getAllMovies();
+  const newMovie = { id: nanoid(), ...data };
+  movies.push(newMovie);
+  reWriteMovies(JSON.stringify(movies, null, 2));
+  return newMovie;
 };
